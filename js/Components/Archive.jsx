7a8d36f6f0808a26510938852504e87e08d0c877
Lukasz <rejlukasz@gmail.com>
2019-06-21 00:48:41 +0200

    Archive component - work in progress

--- js/Components/Archive.jsx
+++ js/Components/Archive.jsx
@@ -5,32 +5,36 @@ function Archive(props) {
 
     console.log(props.history, 'history (archive)');
 
+
     return (
-        <div className="addReport__container">
+        <div className="archive__container">
             <h3>Archive</h3>
             <p>Here is a site with your added daily reports. &#x1F525;</p>
-            <ul>{props.history.map((el) => {
-                return (
-                    <div>
-                        <Link to={`/archive/${el.date}`}>
-                            <p>{el.date && el.date}</p>
-                            {el.eatenFood.breakfast && el.eatenFood.breakfast.map((element) => {
-                                return <p>{element[0].food.label}</p>
-                            })}
-                            {el.eatenFood.lunch && el.eatenFood.lunch.map((element) => {
-                                return <p>{element[0].food.label}</p>
-                            })}
-                            {el.eatenFood.dinner && el.eatenFood.dinner.map((element) => {
-                                return <p>{element[0].food.label}</p>
-                            })}
-                            <p>Your daily calorie intake is: {el.total && el.total} kcal from {props.dailyNeed}</p>
-                            <p>Daily calorie intake to daily calorie need:
-                                { Number((Math.ceil(el.total) / props.dailyNeed) * 100).toFixed(2) + "%"}</p>
-                        </Link>
-                    </div>
-                )
-            })}
-            </ul>
+
+            <div className="archive__panels">
+                {props.history.map((el) => {
+                    return (
+                        <div className="archive__single">
+                            <Link to={`/archive/${el.date}`}>
+                                <h3>{el.date && el.date}</h3>
+                                {el.eatenFood.breakfast && el.eatenFood.breakfast.map((element) => {
+                                    return <p>{element[0].food.label}</p>
+                                })}
+                                {el.eatenFood.lunch && el.eatenFood.lunch.map((element) => {
+                                    return <p>{element[0].food.label}</p>
+                                })}
+                                {el.eatenFood.dinner && el.eatenFood.dinner.map((element) => {
+                                    return <p>{element[0].food.label}</p>
+                                })}
+                                <p>Calorie intake: {el.total && el.total} kcal</p>
+                                <p>Calorie intake to calorie need: {Number((Math.ceil(el.total) / props.dailyNeed)
+                                    * 100).toFixed(2) + "%"}</p>
+                            </Link>
+                        </div>
+                    )
+                })}
+            </div>
+
         </div>
     )
 }
